d0216b96e77153e3bc295f92e0c6fde0907e7f22
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-02-06 10:05:00 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# M1_PERI
+Gestion des périphériques - UE M1 S2
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#/users/enseig/jpeeters/m1.peri/export_rpi_toolchain.sh
+export HOME_IP=132.227.116.42
+#export PATH=$PATH:/home/toumlilt/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin
+export PATH=$PATH:/tmp/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
+
